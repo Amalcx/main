440e95f24311566d75f48c0304e031991390d428
Amalcx <49452746+Amalcx@users.noreply.github.com>
2019-04-10 10:23:27 +0300

    main.py
    assignment one (python)

---
+++ main.py
@@ -0,0 +1,42 @@
+"""
+python is easy assignment-one i don't listen to songs but i found this song
+and this is as many facts i could found on wikipedia
+"""
+
+# using strings first
+song = "Rude"
+artist = "MAGIC"
+album = "Don't kill the magic"
+genre = "pop"
+published = "sony"
+songWritter = "nasri atweh, adam massinger"
+producer = "adam messinger"
+
+
+
+# using numbers
+
+released = 2014
+recorded = 2012
+
+
+# float
+
+# the length of the song
+length = 3.45
+
+#the length of the remix
+rLength = 4.33
+# print selection
+
+print(song)
+print(artist)
+print(album)
+print(genre)
+print(published)
+print(songWritter)
+print(producer)
+print(recorded)
+print(released)
+print(length)
+print(rLength)
